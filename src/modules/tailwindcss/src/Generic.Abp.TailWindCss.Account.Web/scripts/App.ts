ff4997d9b5cc43d1c9bc1467ef8a77a1e41b8ac9
tianxiaode <tianxiaode@hotmail.com>
2024-05-05 21:34:50 +0800

    将BeforeTargets修改为"PreBuildEvent"解决编译不会带上最新js包的问题

--- src/modules/tailwindcss/src/Generic.Abp.TailWindCss.Account.Web/scripts/App.ts
+++ src/modules/tailwindcss/src/Generic.Abp.TailWindCss.Account.Web/scripts/App.ts
@@ -6,6 +6,7 @@ export default class App {
 
     initInput() {
         const els = document.querySelectorAll('input[data-role=input]');
+        console.log('测试生成22222224444444444555555555')
         els.forEach( (el : Element) => {
             new Input(el as HTMLInputElement);
         })
