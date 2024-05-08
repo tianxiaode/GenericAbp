573f619d6dc7c259cd4a5d828d85201fd65ac36d
tianxiaode <tianxiaode@hotmail.com>
2024-05-08 11:03:41 +0800

    完成登出

---
+++ src/modules/tailwindcss/src/Generic.Abp.TailWindCss.Account.Web/Pages/Account/LoggedOut.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NUglify.Helpers;
+
+namespace Generic.Abp.TailWindCss.Account.Web.Pages.Account;
+
+public class LoggedOutModel : AccountPageModel
+{
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public string ClientName { get; set; }
+
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public string SignOutIframeUrl { get; set; }
+
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public string PostLogoutRedirectUri { get; set; }
+
+    public virtual async Task<IActionResult> OnGetAsync()
+    {
+        await NormalizeUrlAsync();
+        return Page();
+    }
+
+    public virtual async Task<IActionResult> OnPostAsync()
+    {
+        await NormalizeUrlAsync();
+        return Page();
+    }
+
+    protected virtual async Task NormalizeUrlAsync()
+    {
+        if (!PostLogoutRedirectUri.IsNullOrWhiteSpace())
+        {
+            PostLogoutRedirectUri = Url.Content(await GetRedirectUrlAsync(PostLogoutRedirectUri));
+        }
+
+        if (!SignOutIframeUrl.IsNullOrWhiteSpace())
+        {
+            SignOutIframeUrl = Url.Content(await GetRedirectUrlAsync(SignOutIframeUrl));
+        }
+    }
+}
